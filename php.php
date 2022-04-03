c0b66fdc5ac65680f3e8cce81fe9cde80bec105c
Giovanna Nunes <giovanna10nunes@gmail.com>
2022-04-02 21:33:56 -0300

    primeira versão

---
+++ php.php
@@ -0,0 +1,25 @@
+<?php
+
+    $nome = $_POST['nome'];
+    $formacao = $_POST['formacao'];
+    $datanasc = $_POST['datanasc'];
+    $nacionalidade = $_POST['nacionalidade'];
+    $endereco = $_POST['endereco'];
+    $email = $_POST['email'];
+    $fone = $_POST['fone'];
+    $experiencia = $_POST['experiencia'];
+    $texto = $_POST['texto'];
+    echo $nome;
+    echo "<br>".$formacao;
+    echo "<br>".$datanasc;
+    echo "<br>".$nacionalidade;
+    echo "<br>".$endereco;
+    echo "<br>".$email;
+    echo "<br>".$fone;
+    echo "<br>".$experiencia;
+    echo "<br>".$texto;
+
+
+
+?>
+        
